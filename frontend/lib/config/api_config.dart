f9f643128800da298c0847f4ba434c1245300c26
PoojyaSenevirathne <poojyaimalsha@gmail.com>
2026-05-21 14:57:02 +0530

    Add login and register screens with auth backend

--- frontend/lib/config/api_config.dart
+++ frontend/lib/config/api_config.dart
@@ -1,4 +1,3 @@
 class ApiConfig {
-  static const String baseUrl =
-      'https://c420884a-0450-4169-bf45-f928ae8e0921-dev.e1-us-east-azure.choreoapis.dev/smarttransit/smarttransit-api/v1.0/api/v1';
+  static const String baseUrl = 'http://localhost:5040/api/v1';
 }
